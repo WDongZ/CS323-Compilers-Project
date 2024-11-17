e10bdfbc5f82697baa92788134374f4ac809f3f8
WhatWEat <1053246332@qq.com>
2024-11-18 00:19:02 +0800

    add outputstream

--- Project 1/Node.hpp
+++ Project 1/Node.hpp
@@ -59,18 +59,86 @@ enum NodeType {
     Args    
 };
 
+std::string to_string(NodeType type) {
+    switch (type) {
+        case Type: return "Type";
+        case Int: return "Int";
+        case Float: return "Float";
+        case Char: return "Char";
+        case Id: return "Id";
+        case If: return "If";
+        case Else: return "Else";
+        case While: return "While";
+        case Return: return "Return";
+        case Dot: return "Dot";
+        case Semi: return "Semi";
+        case Comma: return "Comma";
+        case Assign: return "Assign";
+        case Lt: return "Lt";
+        case Le: return "Le";
+        case Gt: return "Gt";
+        case Ge: return "Ge";
+        case Ne: return "Ne";
+        case Eq: return "Eq";
+        case Plus: return "Plus";
+        case Minus: return "Minus";
+        case Mul: return "Mul";
+        case Div: return "Div";
+        case And: return "And";
+        case Or: return "Or";
+        case Not: return "Not";
+        case Lp: return "Lp";
+        case Rp: return "Rp";
+        case Lb: return "Lb";
+        case Rb: return "Rb";
+        case Lc: return "Lc";
+        case Rc: return "Rc";
+        case Program: return "Program";
+        case ExtDefList: return "ExtDefList";
+        case ExtDef: return "ExtDef";
+        case ExtDecList: return "ExtDecList";
+        case Specifier: return "Specifier";
+        case StructSpecifier: return "StructSpecifier";
+        case VarDec: return "VarDec";
+        case FunDec: return "FunDec";
+        case VarList: return "VarList";
+        case ParamDec: return "ParamDec";
+        case CompSt: return "CompSt";
+        case StmtList: return "StmtList";
+        case Stmt: return "Stmt";
+        case DefList: return "DefList";
+        case Def: return "Def";
+        case DecList: return "DecList";
+        case Dec: return "Dec";
+        case Exp: return "Exp";
+        case Args: return "Args";
+        default: return "Unknown";
+    }
+}
+
+std::ostream &operator<<(std::ostream &os, NodeType type) {
+    os << to_string(type);
+    return os;
+}
 class Node {
 private:
     std::variant<std::string, int, float> value;
+
+    void printTree(std::ostream& os, int depth) const {
+        os << std::string(depth * 4, ' ') << getText() << '\n';
+        for (const auto& child : children) {
+            child->printTree(os, depth + 1);
+        }
+    }
 public:
     NodeType type;
     std::vector<Node *> children;
     // factory method
     static Node * makeNode(NodeType type){
-        return new Node(type, "");
+        return new Node(type, to_string(type));
     }
     static Node * makeNode(NodeType type, std::initializer_list<Node *> children){
-        Node * node = new Node(type, "");
+        Node * node = new Node(type, to_string(type));
         for(auto child : children){
             node->children.push_back(child);
         }
@@ -104,28 +172,20 @@ public:
     void setValue(const std::string& text) {
         value = text;
     }
-    // cout
-    friend std::ostream &operator<<(std::ostream &os, const Node &node) {
-        os << "NodeType: " << node.type
-           << ", Value: ";
 
-        std::visit([&os](auto &&arg) {
-            os << arg;
-        }, node.value);
-
-        if (!node.children.empty()) {
-            os << ", Children: [";
-            for (size_t i = 0; i < node.children.size(); ++i) {
-                if (node.children[i]) {
-                    os << *node.children[i];
-                }
-                if (i != node.children.size() - 1) {
-                    os << ", ";
-                }
-            }
-            os << "]";
+    std::string getText() const {
+        switch (type) {
+            case NodeType::Int:
+                return std::to_string(std::get<int>(value));
+            case NodeType::Float:
+                return std::to_string(std::get<float>(value));
+            default:
+                return std::get<std::string>(value);
         }
-
+    }
+    // cout
+    friend std::ostream& operator<<(std::ostream& os, const Node& node) {
+        node.printTree(os, 0);
         return os;
     }
 };
