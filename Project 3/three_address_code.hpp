d9a34200966e843567282dc7aad11cc0fa507a0a
WhatWEat <1053246332@qq.com>
2024-12-23 20:09:17 +0800

    change every variableaddrees to  a pointer

--- Project 3/three_address_code.hpp
+++ Project 3/three_address_code.hpp
@@ -47,7 +47,7 @@ namespace tac
     {
     public:
         std::string instructionType;
-        bool swap_flag = false;
+        bool swap_flag = false; // 不知道是什么意思，可能if会用一下，但据gpt说用来交换左右操作数？如果只在if利用，你就把这个单独放到if里
 
         TAC(const std::string& type): instructionType(type), swap_flag(false) {}
         virtual ~TAC() = default;
@@ -79,7 +79,7 @@ namespace tac
     // TODO: finish Dec class
     class Dec : public TAC {
     public:
-        VarableAddress var;
+        VarableAddress* var;
         std::vector<int> sizes;
         Dec() = default;
 
@@ -87,92 +87,162 @@ namespace tac
     };
     class Assign: public TAC {
     public:
-        VarableAddress left;
-        VarableAddress right;
-        Assign(VarableAddress left_address, VarableAddress right_address) : TAC(std::string("ASSIGN")), left(left_address), right(right_address) {}
-
+        VarableAddress* left;
+        VarableAddress* right;
+        Assign(VarableAddress* left_address, VarableAddress* right_address) : TAC(std::string("ASSIGN")), left(left_address), right(right_address) {}
+        // 析构函数，释放left和right的内存
+        ~Assign() {
+            delete left;
+            left = nullptr;
+            delete right;
+            right = nullptr;
+        }
         std::string to_string() const override;
     };
     class AssignAddr: public TAC {
     public:
-        VarableAddress left;
-        VarableAddress right;
-        AssignAddr(VarableAddress left_address, VarableAddress right_address) : TAC(std::string("ASSIGNADDR")), left(left_address), right(right_address) {}
+        VarableAddress* left;
+        VarableAddress* right;
+        AssignAddr(VarableAddress* left_address, VarableAddress* right_address) : TAC(std::string("ASSIGNADDR")), left(left_address), right(right_address) {}
+        ~AssignAddr() {
+            delete left;
+            left = nullptr;
+            delete right;
+            right = nullptr;
+        }
         std::string to_string() const override;
     };
     class AssignVal: public TAC {
     public:
-        VarableAddress left;
-        VarableAddress right;
-        AssignVal(VarableAddress left_address, VarableAddress right_address) : TAC(std::string("ASSIGNVAL")), left(left_address), right(right_address) {}
+        VarableAddress* left;
+        VarableAddress* right;
+        AssignVal(VarableAddress* left_address, VarableAddress* right_address) : TAC(std::string("ASSIGNVAL")), left(left_address), right(right_address) {}
+        ~AssignVal() {
+            delete left;
+            left = nullptr;
+            delete right;
+            right = nullptr;
+        }
         std::string to_string() const override;
     };
     class Copy: public TAC {
     public:
-        VarableAddress left;
-        VarableAddress right;
-        Copy(VarableAddress left_address, VarableAddress right_address) : TAC(std::string("COPY")), left(left_address), right(right_address) {}
+        VarableAddress* left;
+        VarableAddress* right;
+        Copy(VarableAddress* left_address, VarableAddress* right_address) : TAC(std::string("COPY")), left(left_address), right(right_address) {}
+        ~Copy() {
+            delete left;
+            left = nullptr;
+            delete right;
+            right = nullptr;
+        }
         std::string to_string() const override;
     };
     class Arithmetic: public TAC {
     public:
         Operator op;
-        VarableAddress result;
-        VarableAddress op_left;
-        VarableAddress op_right;
-        Arithmetic(Operator op, VarableAddress result, VarableAddress op_left, VarableAddress op_right) : TAC(std::string("ARITHMETIC")), op(op), result(result), op_left(op_left), op_right(op_right) {}
+        VarableAddress* result;
+        VarableAddress* op_left;
+        VarableAddress* op_right;
+        Arithmetic(Operator op, VarableAddress* result, VarableAddress* op_left, VarableAddress* op_right) : TAC(std::string("ARITHMETIC")), op(op), result(result), op_left(op_left), op_right(op_right) {}
+        ~Arithmetic() {
+            delete result;
+            result = nullptr;
+            delete op_left;
+            op_left = nullptr;
+            delete op_right;
+            op_right = nullptr;
+        }
         std::string to_string() const override;
     };
     class Goto: public TAC {
     public:
         Label * label;
         Goto(Label * label) : TAC(std::string("GOTO")), label(label) {}
+        ~Goto() {
+            delete label;
+            label = nullptr;
+        }
         std::string to_string() const override;
     };
     class If: public TAC {
     public:
         Operator op;
-        VarableAddress left;
-        VarableAddress right;
-        Label * label;
-        If(Operator op, VarableAddress left, VarableAddress right, Label * label) : TAC(std::string("IF")), op(op), left(left), right(right), label(label) {}
+        VarableAddress* left;
+        VarableAddress* right;
+        Label* label;
+        If(Operator op, VarableAddress* left, VarableAddress* right, Label * label) : TAC(std::string("IF")), op(op), left(left), right(right), label(label) {}
+        ~If() {
+            delete left;
+            left = nullptr;
+            delete right;
+            right = nullptr;
+            delete label;
+            label = nullptr;
+        }
         std::string to_string() const override;
     };
     class Return: public TAC {
     public:
-        VarableAddress value;
-        Return(VarableAddress value) : TAC(std::string("RETURN")), value(value) {}
+        VarableAddress* value;
+        Return(VarableAddress* value) : TAC(std::string("RETURN")), value(value) {}
+        ~Return() {
+            delete value;
+            value = nullptr;
+        }
         std::string to_string() const override;
     };
     class Param: public TAC {
     public:
-        VarableAddress var;
-        Param(VarableAddress var) : TAC(std::string("PARAM")), var(var) {}
+        VarableAddress* var;
+        Param(VarableAddress* var) : TAC(std::string("PARAM")), var(var) {}
+        ~Param() {
+            delete var;
+            var = nullptr;
+        }
         std::string to_string() const override;
     };
     class Arg: public TAC {
     public:
-        VarableAddress right;
-        Arg(VarableAddress right) : TAC(std::string("ARG")), right(right) {}
+        VarableAddress* right;
+        Arg(VarableAddress* right) : TAC(std::string("ARG")), right(right) {}
+        ~Arg() {
+            delete right;
+            right = nullptr;
+        }
         std::string to_string() const override;
     };
     class Call: public TAC {
     public:
-        VarableAddress result;
+        VarableAddress* result;
         Function * function;
-        Call(VarableAddress result, Function * function) : TAC(std::string("CALL")), result(result), function(function) {}
+        Call(VarableAddress* result, Function * function) : TAC(std::string("CALL")), result(result), function(function) {}
+        ~Call() {
+            delete result;
+            result = nullptr;
+            delete function;
+            function = nullptr;
+        }
         std::string to_string() const override;
     };
     class Read: public TAC {
     public:
-        VarableAddress var;
-        Read(VarableAddress var) : TAC(std::string("READ")), var(var) {}
+        VarableAddress* var;
+        Read(VarableAddress* var) : TAC(std::string("READ")), var(var) {}
+        ~Read() {
+            delete var;
+            var = nullptr;
+        }
         std::string to_string() const override;
     };
     class Write: public TAC {
     public:
-        VarableAddress var;
-        Write(VarableAddress var) : TAC(std::string("WRITE")), var(var) {}
+        VarableAddress* var;
+        Write(VarableAddress* var) : TAC(std::string("WRITE")), var(var) {}
+        ~Write() {
+            delete var;
+            var = nullptr;
+        }
         std::string to_string() const override;
     };
 }
